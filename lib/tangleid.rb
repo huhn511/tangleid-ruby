82b983f5c9cbe2e086cfcb8197ae01161281bc70
Unknown <sehe89@gmail.com>
2018-12-08 18:53:53 +0100

    add tests and refactor

--- lib/tangleid.rb
+++ lib/tangleid.rb
@@ -13,11 +13,11 @@ module TangleID
       setSettings(settings)
     end
 
-    def new_user(first_name = '', last_name = '')
+    def new_user(first_name:, last_name:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/new_user.sh
       # POST '{"command":"new_user","first_name":"Huang","cosignerp":"SD9BCRDGJYWDHPTDNOPRULFWWG","cosigners":"SD9BCRDGJYWDHPTDNOPRULFWWG","last_name":"JyunYu","profile_picture":"https://s3-us-west-1.amazonaws.com/niusnews-imgs/146716_5.jpg","uuid":"ED9BCRDGJYWDHPTDNOPRULFWWG","pk":"SD9BCRDGJYWDHPTDNOPRULFWWG"}'
 
-      obj = {
+      data = {
         "command":"new_user",
         "first_name": first_name,
         "cosignerp":"SD9BCRDGJYWDHPTDNOPRULFWWG",
@@ -28,120 +28,120 @@ module TangleID
         "pk":"SD9BCRDGJYWDHPTDNOPRULFWWG"
       }
 
-      return send obj
+      return send data
     end
 
-    def login(uuid)
+    def login(uuid:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/login.sh
       # POST '{"command":"login","uuid": "ED9BCRDGJYWDHPTDNOPRULFWWG"}'
-      obj = {
+      data = {
         "command":"login",
         "uuid": uuid
       }
 
-      return send obj
+      return send data
     end
 
-    def new_group
+    def new_group(uuid:, first_name:, last_name:, cosignerp:, cosigners:, profile_picture:, pk:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/new_group.sh
       # POST '{"command":"new_group","first_name":"Huang", "cosignerp":"SD9BCRDGJYWDHPTDNOPRULFWWG","cosigners":"SD9BCRDGJYWDHPTDNOPRULFWWG", "last_name":"JyunYu","profile_picture":"https://s3-us-west-1.amazonaws.com/niusnews-imgs/146716_5.jpg", "uuid":"ED9BCRDGJYWDHPTDNOPRULFWWG","pk":"SD9BCRDGJYWDHPTDNOPRULFWWG"}'
-      obj = {
-        "command":"new_group",
-        "first_name":"Huang",
-        "cosignerp":"SD9BCRDGJYWDHPTDNOPRULFWWG",
-        "cosigners":"SD9BCRDGJYWDHPTDNOPRULFWWG",
-        "last_name":"JyunYu",
-        "profile_picture":"https://s3-us-west-1.amazonaws.com/niusnews-imgs/146716_5.jpg",
-        "uuid":"ED9BCRDGJYWDHPTDNOPRULFWWG",
-        "pk":"SD9BCRDGJYWDHPTDNOPRULFWWG"
+      data = {
+        "command": "new_group",
+        "first_name": first_name,
+        "cosignerp": cosignerp,
+        "cosigners": cosigners,
+        "last_name": last_name,
+        "profile_picture": profile_picture,
+        "uuid": uuid,
+        "pk": pk
       }
 
-      return send obj
+      return send data
     end
 
-    def get_all_claims
+    def get_all_claims(uuid:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/get_all_claims.sh
       # POST '{"command":"get_all_claims","uuid": "V9TCFLAOGGTAQATTJBLABAG9WY"}'
-      obj = {
+      data = {
         "command":"get_all_claims",
         "uuid": "V9TCFLAOGGTAQATTJBLABAG9WY"
       }
 
-      return send obj
+      return send data
     end
 
 
-    def get_claim_info
+    def get_claim_info(hash_txn:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/get_claim_info.sh
       # POST '{"command":"get_claim_info","hash_txn": "FTFJEAKZTBBFWLD9TWFYLCZABKTMKD9QKYWOZLNMOMNRKCLTNSDAXNFUODFQSSSNRGQTMPMFGPFF99999"}'
-       obj = {
-         "command":"get_claim_info",
-         "hash_txn": "FTFJEAKZTBBFWLD9TWFYLCZABKTMKD9QKYWOZLNMOMNRKCLTNSDAXNFUODFQSSSNRGQTMPMFGPFF99999"
+       data = {
+         "command": "get_claim_info",
+         "hash_txn": hash_txn
        }
 
-       return send obj
+       return send data
     end
 
-    def new_claim
+    def new_claim(uuid:, part_a:, part_b:, exp_date:, claim_pic:, msg:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/new_claim.sh
       # POST '{"command":"new_claim","uuid": "SD9BCRDGJYWDHPTDNOPRULFWWG","part_a":"9JVXCXMFAMKLUQQCDACSWJVDLH","part_b":"9JVXCXMFAMKLUQQCDACSWJVDLH","exp_date":"20190101","claim_pic":"https://www.chill.ie/images/Customer-Care/ncb-example.png","msg":"testing message"}'
-      obj = {
+      data = {
         "command":"new_claim",
-        "uuid": "SD9BCRDGJYWDHPTDNOPRULFWWG",
-        "part_a":"9JVXCXMFAMKLUQQCDACSWJVDLH",
-        "part_b":"9JVXCXMFAMKLUQQCDACSWJVDLH",
-        "exp_date":"20190101",
-        "claim_pic":"https://www.chill.ie/images/Customer-Care/ncb-example.png",
-        "msg":"testing message"
+        "uuid": uuid,
+        "part_a": part_a,
+        "part_b": part_b,
+        "exp_date": exp_date,
+        "claim_pic": claim_pic,
+        "msg": msg
       }
-      return send obj
+      return send data
     end
 
-    def revoke_claim
+    def revoke_claim(uuid:, txnhash:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/revoke_claim.sh
       # POST '{"command":"revoke_claim","uuid": "SD9BCRDGJYWDHPTDNOPRULFWWG","txnhash":"KQD9IKGNUM9ZVMHJHKVZLAAVDNDWJRNTZDYB9SXKMXPMBYNRGCOIMIVLTSRCJEXRAMWDNZODLBVR99999"}'
-      obj = {
+      data = {
         "command":"revoke_claim",
         "uuid": "SD9BCRDGJYWDHPTDNOPRULFWWG",
         "txnhash":"KQD9IKGNUM9ZVMHJHKVZLAAVDNDWJRNTZDYB9SXKMXPMBYNRGCOIMIVLTSRCJEXRAMWDNZODLBVR99999"
       }
 
-      return send obj
+      return send data
     end
 
-    def get_all_revoke_claims
+    def get_all_revoke_claims(uuid:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/get_all_revoke_claims.sh
       # POST '{"command":"get_all_revoke_claims","uuid": "SD9BCRDGJYWDHPTDNOPRULFWWG"}'
-      obj = {
+      data = {
         "command":"get_all_revoke_claims",
-        "uuid": "SD9BCRDGJYWDHPTDNOPRULFWWG"
+        "uuid": uuid
       }
 
-      return send obj
+      return send data
     end
 
-    def get_all_notifies
+    def get_all_notifies(uuid:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/get_all_notifies.sh
       # POST '{"command":"get_all_notifies","uuid": "SD9BCRDGJYWDHPTDNOPRULFWWG"}'
-      obj = {
+      data = {
         "command":"get_all_notifies",
-        "uuid": "SD9BCRDGJYWDHPTDNOPRULFWWG"
+        "uuid": uuid
       }
 
-      return send boobjdy
+      return send data
     end
 
-    def send_notify(uuid, receiver, message)
+    def send_notify(uuid:, receiver:, message:)
       # https://github.com/TangleID/tangleid-api-examples/blob/master/send_notify.sh
       # POST '{"command":"send_notify","uuid":"SD9BCRDGJYWDHPTDNOPRULFWWG","receiver": "SD9BCRDGJYWDHPTDNOPRULFWWG","message":"HAPPYBIRTHDAY"}'
-      obj = {
-        "command":"send_notify",
-        "uuid":uuid,
+      data = {
+        "command": "send_notify",
+        "uuid": uuid,
         "receiver": receiver,
         "message": message
       }
 
-      return send obj
+      return send data
     end
 
     private
@@ -156,21 +156,19 @@ module TangleID
       hash.inject({}){ |h,(k,v)| h[k.to_sym] = v; h }
     end
 
-    def send(obj)
+    def send(data)
+
       uri = URI.parse(@swarm_node_url)
       https = Net::HTTP.new(uri.host,uri.port)
       # TODO: comment in line blow
       #https.use_ssl = true
       req = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-      req.body = obj.to_json
+      req.body = data.to_json
       res = https.request(req)
-      puts "Response #{res.code} #{res.message}}"
-
+      
       if res.code == "200"
-        puts "Success! Hash: #{res.body}"
         return res.body
       else
-        pp "Error: #{res.body}"
         return false
       end
     end
--- spec/tangleid_spec.rb
+++ spec/tangleid_spec.rb
@@ -8,11 +8,11 @@ describe TangleID do
 
   context 'new_user' do
     it 'with valid inputs should return a hash' do
-      @valid_input = {
+      valid_input = {
         first_name: "First",
         last_name: "Last"
       }
-      response = @api.new_user(@valid_input)
+      response = @api.new_user(valid_input)
       expect(response).not_to be("")
       expect(response).not_to be(false)
       expect(response.length).to be(81)
@@ -22,9 +22,11 @@ describe TangleID do
   context 'login' do
     it 'with valid inputs should return a hash' do
 
-      valid_uuid = "ED9BCRDGJYWDHPTDNOPRULFWWG"
+      valid_input = {
+        uuid: "ED9BCRDGJYWDHPTDNOPRULFWWG"
+      }
 
-      response = @api.login(valid_uuid)
+      response = @api.login(valid_input)
 
       expect(response).not_to be("")
       expect(response).not_to be(false)
@@ -33,11 +35,181 @@ describe TangleID do
 
     it 'with invalid inputs should return a hash' do
 
-      invalid_uuid = "INVALIDUUID"
+      invalid_input = {
+        uuid: "INVALIDUUID"
+      }
 
-      response = @api.login(invalid_uuid)
+      response = @api.login(invalid_input)
 
       expect(response.include?("'hashes': []")).to be(true)
     end
   end
+
+  context 'new_group' do
+    it 'with valid inputs should return a hash' do
+
+      valid_input = {
+        uuid: "ED9BCRDGJYWDHPTDNOPRULFWWG",
+        first_name: "First",
+        last_name: "Last",
+        cosignerp: "SD9BCRDGJYWDHPTDNOPRULFWWG",
+        cosigners: "SD9BCRDGJYWDHPTDNOPRULFWWG",
+        profile_picture: "https://s3-us-west-1.amazonaws.com/niusnews-imgs/146716_5.jpg",
+        pk: "SD9BCRDGJYWDHPTDNOPRULFWWG"
+      }
+
+      response = @api.new_group(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+      expect(response.length).to be(81)
+    end
+  end
+
+  context 'get_all_claims' do
+    it 'with valid inputs should return all claims' do
+
+      valid_input = {
+        uuid: "V9TCFLAOGGTAQATTJBLABAG9WY"
+      }
+
+      response = @api.get_all_claims(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+    end
+  end
+
+  context 'get_claim_info' do
+    it 'with valid hash_txn should return a ?' do
+
+      valid_input = {
+        hash_txn: "FTFJEAKZTBBFWLD9TWFYLCZABKTMKD9QKYWOZLNMOMNRKCLTNSDAXNFUODFQSSSNRGQTMPMFGPFF99999"
+      }
+
+      response = @api.get_claim_info(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+    end
+    it 'with invalid hash_txn should return a ?' do
+
+      valid_input = {
+        hash_txn: "INVALIDZTBBFWLD9TWFYLCZABKTMKD9QKYWOZLNMOMNRKCLTNSDAXNFUODFQSSSNRGQTMPMFGPFF99999"
+      }
+
+      response = @api.get_claim_info(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+    end
+  end
+
+  context 'new_claim' do
+    it 'with valid inputs should return a hash' do
+
+      valid_input = {
+        uuid: "SD9BCRDGJYWDHPTDNOPRULFWWG",
+        part_a: "9JVXCXMFAMKLUQQCDACSWJVDLH",
+        part_b: "9JVXCXMFAMKLUQQCDACSWJVDLH",
+        exp_date: "20190101",
+        claim_pic: "https://www.chill.ie/images/Customer-Care/ncb-example.png",
+        msg: "testing message"
+      }
+
+      response = @api.new_claim(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+      expect(response.length).to be(81)
+    end
+  end
+
+  context 'revoke_claim' do
+    it 'with valid inputs should return a hash' do
+
+      valid_input = {
+        uuid: "SD9BCRDGJYWDHPTDNOPRULFWWG",
+        txnhash: "KQD9IKGNUM9ZVMHJHKVZLAAVDNDWJRNTZDYB9SXKMXPMBYNRGCOIMIVLTSRCJEXRAMWDNZODLBVR99999"
+      }
+
+      response = @api.revoke_claim(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+      expect(response.length).to be(81)
+    end
+  end
+
+  context 'get_all_revoke_claims' do
+    it 'should return a array with hashes with valid inputs ' do
+
+      valid_input = {
+        uuid: "SD9BCRDGJYWDHPTDNOPRULFWWG"
+      }
+
+      response = @api.get_all_revoke_claims(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+      expect(response.include?("'hashes': []")).to_not be(true)
+    end
+    it 'with invalid inputs should return an empty array' do
+
+      valid_input = {
+        uuid: "INVALID"
+      }
+
+      response = @api.get_all_revoke_claims(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+      expect(response.include?("'hashes': []")).to be(true)
+    end
+  end
+
+  context 'get_all_notifies' do
+    it 'with valid inputs should return an array with hashes' do
+
+      valid_input = {
+        uuid: "SD9BCRDGJYWDHPTDNOPRULFWWG"
+      }
+
+      response = @api.get_all_notifies(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+      expect(response.include?("'hashes': []")).to_not be(true)
+    end
+    it 'with invalid inputs should return an empty array' do
+
+      valid_input = {
+        uuid: "INVALID"
+      }
+
+      response = @api.get_all_notifies(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+      expect(response.include?("'hashes': []")).to be(true)
+    end
+  end
+
+  context 'send_notify' do
+    it 'with valid inputs should return a hash' do
+
+      valid_input = {
+        uuid: "SD9BCRDGJYWDHPTDNOPRULFWWG",
+        receiver: "SD9BCRDGJYWDHPTDNOPRULFWWG",
+        message: "test message"
+      }
+
+      response = @api.send_notify(valid_input)
+
+      expect(response).not_to be("")
+      expect(response).not_to be(false)
+      expect(response.length).to be(81)
+    end
+  end
+
 end
